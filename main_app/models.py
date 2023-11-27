5f2f3a797ab083fc1b9253dd7de5ab68ccec47dc
NickSaltamachia <nsaltamachia@gmail.com>
2023-11-26 20:09:05 -0600

    adds __str__ method to Restaurant model

--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,6 @@ class Restaurant(models.Model):
     address = models.CharField(max_length=150)
     neighborhood = models.TextField(max_length=150)
     cuisine = models.TextField(max_length=150)
+
+    def __str__(self):
+        return self.name
