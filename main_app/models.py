a2cd419f98f9f7a7f5f76196cfe4cfa6e30d0b68
NickSaltamachia <nsaltamachia@gmail.com>
2023-11-26 20:07:58 -0600

    adds restaurant model in models.py, migrates new model to database p3, adds restaurant instance Alinea (via python shell)

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
+    neighborhood = models.TextField(max_length=150)
+    cuisine = models.TextField(max_length=150)
