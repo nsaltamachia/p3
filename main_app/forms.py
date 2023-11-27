77a2fd0c72b5ef576749f23d14e9d7a4de3ed750
Zachary Mitchell <masspike84@yahoo.com>
2023-11-27 15:20:37 -0500

    Create Meal Had form and forms.py

---
+++ main_app/forms.py
@@ -0,0 +1,7 @@
+from django.forms import ModelForm
+from .models import Meal_Had
+
+class Meal_Had_Form(ModelForm):
+    class Meta:
+        model = Meal_Had
+        fields = ['date', 'description', 'restaurant']
