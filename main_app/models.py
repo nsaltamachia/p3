fe158c8b4fbd71be0be5bbc6cd1887da5452514c
NickSaltamachia <nsaltamachia@gmail.com>
2023-11-27 13:16:12 -0600

    adds Comments model to models.py and migrates

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -14,4 +15,15 @@ class Restaurant(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'restaurant_id': self.id})
+    
+class Comment(models.Model):
+    comment = models.TextField(max_length=400)
+    created_at = models.DateTimeField(auto_now_add=True)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    upvotes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.post}"
     
