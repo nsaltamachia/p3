f57ca66853600f6d7e0012e1e459043cb3a47c5c
K Castillo <krystalin.castillo@gmail.com>
2023-11-28 14:36:34 -0600

    All models complete

--- main_app/models.py
+++ main_app/models.py
@@ -6,42 +6,6 @@ from datetime import date
 
 # Create your models here.
 
-class Restaurant(models.Model):
-    name = models.CharField(max_length=150)
-    address = models.CharField(max_length=150)
-    neighborhood = models.TextField(max_length=150)
-    cuisine = models.TextField(max_length=150)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-    
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'restaurant_id': self.id})
-
-class MealPhoto(models.Model):
-    url = models.CharField(max_length=200)
-    meal_had = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"photo for meal_had_id: {self.meal_had_id} @{self.url}"
-
-class Meal_Had(models.Model):
-    date = models.DateField('meal date')
-    description = models.TextField(max_length=250)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.description} on {self.date}'
-           
-class Comment(models.Model):
-    comment = models.TextField(max_length=400)
-    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return f"{self.id}"
-      
-
-
 class Seating(models.Model):
     TYPE_CHOICES = [
         ('indoor', 'Indoor'),
@@ -55,6 +19,42 @@ class Seating(models.Model):
     type = models.CharField(max_length=150)
     indoor = models.CharField(max_length=20, choices=TYPE_CHOICES)
     handicap = models.CharField(max_length=1, choices=YES_NO_CHOICES)
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=150)
+    address = models.CharField(max_length=150)
+    neighborhood = models.TextField(max_length=150)
+    cuisine = models.TextField(max_length=150)
+    seating = models.ManyToManyField(Seating)
+    
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'restaurant_id': self.id})
+
+class Meal_Had(models.Model):
+    date = models.DateField('meal date')
+    description = models.TextField(max_length=250)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.description} on {self.date}'
+
+class MealPhoto(models.Model):
+    url = models.CharField(max_length=200)
+    meal_had = models.ForeignKey(Meal_Had, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"photo for meal_had_id: {self.meal_had_id} @{self.url}"
+           
+class Comment(models.Model):
+    comment = models.TextField(max_length=400)
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.id}"
+    
 
 
 
